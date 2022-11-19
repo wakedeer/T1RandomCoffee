c5052e0714d1599f3c0997d6dd9684d832bee3bc
Миронов Алексей Юрьевич (4052206) <aumironov@vtb.ru>
2022-11-19 16:09:02 +0300

    clean up code

--- src/main/kotlin/inno/tech/handler/question/ShowTopicHandler.kt
+++ src/main/kotlin/inno/tech/handler/question/ShowTopicHandler.kt
@@ -1,6 +1,5 @@
 package inno.tech.handler.question
 
-import inno.tech.TelegramProperties
 import inno.tech.constant.Command
 import inno.tech.constant.Message
 import inno.tech.exception.RandomCoffeeBotException
@@ -19,21 +18,20 @@ import org.telegram.telegrambots.meta.api.objects.Update
  *
  * @param messageService сервис отправки сообщений
  * @param topicRepository репозиторий для работы с топиками вопросов
- * @param topicRepository репозиторий для работы с вопросами
+ * @param questionsRepository репозиторий для работы с вопросами
  */
 @Component
 class ShowTopicHandler(
     private val messageService: MessageService,
     private val topicRepository: TopicRepository,
     private val questionsRepository: QuestionsRepository,
-    private val telegramProperties: TelegramProperties,
 ) : Handler {
     override fun accept(command: String, user: User?): Boolean {
         return command.startsWith(Command.SHOW_QUESTIONS.command)
     }
 
     override fun handle(update: Update, user: User?) {
-        val topicId = extractTopicId(user, update)
+        val topicId = extractTopicId(update)
         val topic = topicRepository.findById(topicId).toNullable() ?: throw RandomCoffeeBotException("Topic $topicId cannot be found")
         val questions = questionsRepository.findAllByTopic(topic)
         val questionText = questions.asSequence()
@@ -49,7 +47,7 @@ class ShowTopicHandler(
      * @param update входящее сообщение
      * @return идентификатор топика
      */
-    private fun extractTopicId(user: User?, update: Update): Long =
+    private fun extractTopicId(update: Update): Long =
         update.callbackQuery?.data?.split("/")?.last()?.toLong()
             ?: throw RandomCoffeeBotException("TopicId cannot be extracted from message: $update")
 }
